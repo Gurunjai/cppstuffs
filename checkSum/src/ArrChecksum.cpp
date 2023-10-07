4583c1e82e54c57ace5803b376b46d7d967139b1
Guru K <guruper@gmail.com>
2023-10-06 18:36:53 -0700

    arrchecksum

--- checkSum/src/ArrChecksum.cpp
+++ checkSum/src/ArrChecksum.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include <cassert>
 #include <boost/crc.hpp>
 
 using namespace std;
@@ -36,13 +37,8 @@ int main (void)
         a2.push_back(i);
     }
 
-    for (int i = 0; i < 10; ) {
-        ++i;
-        if ((i % 3) == 0) {
-            a3.push_back(i * 9);
-        } else {
-            a3.push_back(i);
-        }
+    for (int i = 10; i > 0; ) {
+        a3.push_back(i--);
     }
 
     vector<int> a4 = a2;
@@ -52,6 +48,9 @@ int main (void)
     cout << "Array-3:\t" << dumpArr(a3);
     cout << "Array-4:\t" << dumpArr(a4);
 
+    assert(computeCrcCheckSum(a1.data(), a1.size()) != computeCrcCheckSum(a2.data(), a2.size()));
+    assert(computeCrcCheckSum(a2.data(), a2.size()) != computeCrcCheckSum(a3.data(), a3.size()));
+    assert(computeCrcCheckSum(a2.data(), a2.size()) == computeCrcCheckSum(a4.data(), a4.size()));
     cout << "Array-1 CheckSum= " << computeCrcCheckSum(a1.data(), a1.size()) << endl;
     cout << "Array-2 CheckSum= " << computeCrcCheckSum(a2.data(), a2.size()) << endl;
     cout << "Array-3 CheckSum= " << computeCrcCheckSum(a3.data(), a3.size()) << endl;
